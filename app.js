078821b75d516ef45e3259e812a37efde1682fe9
Michael Lopez <michael.94lopez@gmail.com>
2022-06-22 15:15:18 -0500

    styled CSS, added placehoder code for JS

--- app.js
+++ app.js
@@ -6,3 +6,28 @@
     [null, null, null]
   ]
 }
+
+
+
+// state
+let initialState;
+
+function buildInitialState() {
+
+}
+
+// render
+function renderState() {
+
+}
+
+// maybe a dozen or so helper functions for tiny pieces of the interface
+
+// listeners
+function onBoardClick() {
+  // update state, maybe with another dozen or so helper functions...
+
+  renderState() // show the user the new state
+}
+const board = document.getElementById('board');
+board.addEventListener('click', onBoardClick); 
